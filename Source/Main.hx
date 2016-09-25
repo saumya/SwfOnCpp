41c06ae24ee1eac91c9dbe08e767dc48c87f2e2a
saumya <2saumyaray@gmail.com>
2016-09-25 13:15:12 +0530

    testing done with stop and play of a movieClip in CPP and HTML5 target

--- Source/Main.hx
+++ Source/Main.hx
@@ -2,29 +2,56 @@ package;
 
 
 import openfl.display.Sprite;
+import openfl.events.MouseEvent;
+
 
 
 class Main extends Sprite {
 	
+	private var animRect:AnimRect;
+	private var isAnimating:Bool;
 	
 	public function new () {
 		
 		super ();
 		
+		this.isAnimating = false;
 		drawFromSwf();
 		
 	}
 
 	private function drawFromSwf():Void{
-		var clip = new BlueRect ();
-		addChild(clip);
+		var btn = new BlueRect ();
+		btn.x = 10;
+		btn.y = 10;
 
-		clip.x = 10;
-		clip.y = 10;
+		//btn.width = 50;
+		//btn.height = 25;
+		//btn.scaleX = btn.scaleY = 0.5;
 
-		//clip.width = 50;
-		//clip.height = 25;
-		clip.scaleX = clip.scaleY = 0.5;
+		btn.addEventListener(MouseEvent.CLICK,onBtnClick);
+
+		//var animRect = new AnimRect();
+		this.animRect = new AnimRect();
+		this.animRect.x = 10;
+		this.animRect.y = 60;
+		
+		this.animRect.stop();
+
+		// Add the UI
+		addChild(btn);
+		addChild(this.animRect);
+	}
+
+	private function onBtnClick(e:MouseEvent):Void{
+		trace("onBtnClick");
+		if (this.isAnimating) {
+			this.animRect.stop();
+			this.isAnimating = false;
+		}else{
+			this.animRect.play();
+			this.isAnimating = true;
+		}
 	}
 	
 	
