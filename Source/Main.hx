e91930c8d827b8d409dcc8b493fdeb7c7d3dc361
saumya <2saumyaray@gmail.com>
2016-09-27 20:08:15 +0530

    code cleanup

--- Source/Main.hx
+++ Source/Main.hx
@@ -50,6 +50,7 @@ class Main extends Sprite {
 
 	private function onStageResize(e:Event):Void{
 		trace('onStageResize',this.stage.stageWidth,this.stage.stageHeight);
+		
 		this.centerTheGameOnStage();
 		this.gameView.onStageResize();
 	}
