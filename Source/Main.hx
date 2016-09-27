51949e4857d6891f4848528ce3162c89e2c494d1
saumya <2saumyaray@gmail.com>
2016-09-27 17:15:03 +0530

    code organisation

--- Source/Main.hx
+++ Source/Main.hx
@@ -6,71 +6,35 @@ import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.events.MouseEvent;
 
+import com.saumya.game.GameEntryView;
+
 
 
 class Main extends Sprite {
-	
-	private var animRect:AnimRect;
-	private var isAnimating:Bool;
 
-	private var gameView:Sprite;
+	private var gameView:GameEntryView;
 	
 	public function new () {
-		
 		super ();
-		
 		this.addEventListener(Event.ADDED_TO_STAGE,onAddedToStage);
-
-		this.gameView = new Sprite();
-		addChild(this.gameView);
-
-		this.isAnimating = false;
 	}
-
 	private function onAddedToStage(e:Event):Void{
-		drawFromSwf();
+		trace('onAddedToStage');
+		this.gameView = new GameEntryView();	
+		this.addChild(this.gameView);
+
+		this.gameView.addEventListener(GameEntryView.UI_READY,onGameUiReady);
+	}
+	private function onGameUiReady(e:Event):Void{
+		trace('onGameUiReady');
+
+		this.centerTheGameOnStage();
 	}
 	private function centerTheGameOnStage():Void{
-		//position the game in center
+		trace(this.gameView.width,this.gameView.height);
+
 		this.gameView.x = (this.stage.stageWidth - this.gameView.width)/2 ;
 		this.gameView.y = (this.stage.stageHeight - this.gameView.height)/2 - 100;
 	}
-
-	private function drawFromSwf():Void{
-		var btn = new BlueRect ();
-		btn.x = 10;
-		btn.y = 10;
-
-		//btn.width = 50;
-		//btn.height = 25;
-		//btn.scaleX = btn.scaleY = 0.5;
-
-		btn.addEventListener(MouseEvent.CLICK,onBtnClick);
-
-		//var animRect = new AnimRect();
-		this.animRect = new AnimRect();
-		this.animRect.x = 10;
-		this.animRect.y = 60;
-		
-		this.animRect.stop();
-
-		// Add the UI
-		this.gameView.addChild(btn);
-		this.gameView.addChild(this.animRect);
-		//
-		this.centerTheGameOnStage();
-	}
-
-	private function onBtnClick(e:MouseEvent):Void{
-		trace("onBtnClick");
-		if (this.isAnimating) {
-			this.animRect.stop();
-			this.isAnimating = false;
-		}else{
-			this.animRect.play();
-			this.isAnimating = true;
-		}
-	}
-	
 	
 }
---
+++ Source/com/saumya/game/GameEntryView.hx
@@ -0,0 +1,85 @@
+package com.saumya.game;
+
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+
+
+
+class GameEntryView extends Sprite {
+	
+	public static var UI_READY:String = "GameUiIsReady";
+
+	private var animRect:AnimRect;
+	private var isAnimating:Bool;
+
+	public function new() {
+		super();
+		this.addEventListener(Event.ADDED_TO_STAGE,onAddedToStage);
+	}
+	private function onAddedToStage(e:Event):Void{
+		this.preInit();
+	}
+	public function preInit():Void{
+		trace('preInit');
+
+		this.isAnimating = false;
+		this.init();
+	}
+	public function init():Void{
+		trace('init');
+
+		this.drawFromSwf();
+	}
+	public function render():Void{
+		trace('render');
+
+		this.drawFromSwf();
+	}
+
+	private function drawFromSwf():Void{
+		trace('drawFromSwf');
+
+		var btn = new BlueRect ();
+		btn.x = 10;
+		btn.y = 10;
+
+		//btn.width = 50;
+		//btn.height = 25;
+		//btn.scaleX = btn.scaleY = 0.5;
+
+		btn.addEventListener(MouseEvent.CLICK,onBtnClick);
+
+		//var animRect = new AnimRect();
+		this.animRect = new AnimRect();
+		this.animRect.x = 10;
+		this.animRect.y = 60;
+		
+		this.animRect.stop();
+
+		// Add the UI
+		this.addChild(btn);
+		this.addChild(this.animRect);
+		//
+		var ge:Event = new Event(GameEntryView.UI_READY,true);
+		this.dispatchEvent(ge);
+		this.centerTheGameOnStage();
+	}
+
+	private function onBtnClick(e:MouseEvent):Void{
+		trace("onBtnClick");
+
+		if (this.isAnimating) {
+			this.animRect.stop();
+			this.isAnimating = false;
+		}else{
+			this.animRect.play();
+			this.isAnimating = true;
+		}
+	}
+
+	private function centerTheGameOnStage():Void{
+		this.x = (this.stage.stageWidth - this.width)/2 ;
+		this.y = (this.stage.stageHeight - this.height)/2;
+	}
+}
