4aeb431a0b88149e99f07881c623d6ae1c71cfd7
saumya <2saumyaray@gmail.com>
2017-02-15 20:18:15 +0530

    playing with different options for the gameEntry

--- Source/com/saumya/game/GameShell.hx
+++ Source/com/saumya/game/GameShell.hx
@@ -12,9 +12,15 @@ package com.saumya.game;
 import openfl.display.Sprite;
 import openfl.display.Graphics;
 
+import openfl.events.Event;
+import openfl.events.MouseEvent;
+
+import openfl.net.SharedObject;
+
 import motion.Actuate;
 
 import com.saumya.raymp.components.ButtonWithBgColor;
+import com.saumya.raymp.components.TextInputWithBgColor;
 
 
 
@@ -24,6 +30,8 @@ class GameShell extends Sprite {
 	private var bg:Sprite;
 	private var gWidth:Float;
 	private var gHeight:Float;
+
+	private var tUserData:TextInputWithBgColor;
 
 	public function new(gameWidth:Float,gameHeight:Float) {
 		super();
@@ -38,13 +46,53 @@ class GameShell extends Sprite {
 		this.addChild(bg);
 		//
 		var g:Graphics = this.bg.graphics;
-		g.beginFill(0xAAAAAA,1.0);
+		g.beginFill(0xCCAAAA,1.0);
 		g.drawRect(0,0,this.gWidth,this.gHeight);
 		g.endFill();
 	}
 	public function init():Void{
-		var b1:ButtonWithBgColor = new ButtonWithBgColor("Version number of the Game",20,0x777777);
+		this.drawUI();
+	}
+	private function drawUI():Void{
+		var b1:ButtonWithBgColor = new ButtonWithBgColor("Version number of the Game - 1.0.0",30,0x777777);
 		this.addChild(b1);
-		b1.x = b1.y = 10;
+		b1.x = b1.y = 40;
+		//
+		var bSave:ButtonWithBgColor = new ButtonWithBgColor("Save Data",50);
+		bSave.x = 40;
+		bSave.y = 100;
+		this.addChild(bSave);
+		//
+		var bGetSaved:ButtonWithBgColor = new ButtonWithBgColor("Get Data",50);
+		bGetSaved.x = 300;
+		bGetSaved.y = 100;
+		this.addChild(bGetSaved);
+		//
+		bSave.addEventListener(MouseEvent.CLICK,onSaveClick);
+		bGetSaved.addEventListener(MouseEvent.CLICK,onGetSavedDataClick);
+		//
+		this.tUserData = new TextInputWithBgColor('user data');
+		tUserData.x = 40;
+		tUserData.y = 200;
+		this.addChild(this.tUserData);
+
+	}
+
+	private function onSaveClick(e:MouseEvent):Void{
+		//trace(this.tUserData.getText());
+
+		var so:SharedObject = SharedObject.getLocal("savedData");
+		var s:String = this.tUserData.getText();
+		//trace('this.resultField.text',s);
+
+		so.setProperty("userData",s);
+		so.flush(100);
+	}
+	private function onGetSavedDataClick(e:MouseEvent):Void{
+		//trace(this.tUserData.getText());
+		var so:SharedObject = SharedObject.getLocal("savedData");
+		trace('so.data.appName',so.data.userData);
+
+		this.tUserData.setText(so.data.userData);
 	}
 }
